edac2e9644212619df4a8b730d4ba8e9dfdc8d2a
AlexRatik <aleksandr-ratkevich@mail.ru>
2022-07-28 18:30:00 +0300

    add portfolioValue in header

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -2,11 +2,13 @@ import React from "react";
 import CurrencyTableau from "../currencyTableau/currencyTableau";
 import "./header.scss";
 import Button from "../button/button";
+import PortfolioValue from "../portfolioValue/portfolioValue";
 
 const Header = () => {
   return (
     <div className="header">
       <CurrencyTableau />
+      <PortfolioValue />
       <Button text={"Portfolio"} clickHandler={() => {}} />
     </div>
   );
