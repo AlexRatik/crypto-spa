c5ef29b448f390e76258be2d188613ac0b944496
AlexRatik <aleksandr-ratkevich@mail.ru>
2022-07-28 21:31:34 +0300

    add pagination to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import Header from "./components/header/header";
 import { getCryptoCoins } from "./features/cryptoCoins/cryptoCoinsSlice";
 import { useAppDispatch } from "./hooks/hooks";
 import Table from "./components/table/table";
+import Pagination from "./components/pagination/pagination";
 
 function App() {
   const dispatch = useAppDispatch();
@@ -15,6 +16,7 @@ function App() {
     <div className="App">
       <Header />
       <Table />
+      <Pagination />
     </div>
   );
 }
