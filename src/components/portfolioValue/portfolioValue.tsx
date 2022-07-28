608f241d83e0571fe29f294f0afb9e83d88761b5
AlexRatik <aleksandr-ratkevich@mail.ru>
2022-07-28 18:29:48 +0300

    add portfolioValue

---
+++ src/components/portfolioValue/portfolioValue.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./portfoliaValue.scss";
+
+const PortfolioValue = () => {
+  return <div className="portfolioValue">134,32 USD +2,38 (1,80 %)</div>;
+};
+
+export default PortfolioValue;
