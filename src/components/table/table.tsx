02944a2ce4aa5a5cd2b954a835645bcdce14d763
AlexRatik <aleksandr-ratkevich@mail.ru>
2022-07-28 21:31:21 +0300

    fix table

--- src/components/table/table.tsx
+++ src/components/table/table.tsx
@@ -4,9 +4,21 @@ import { useAppSelector } from "../../hooks/hooks";
 import { TableColumnsEnum } from "./tableColumnsEnum";
 import "./table.scss";
 import Table__Container from "./table__Container/table__Container";
+import { usePagination } from "../../hooks/usePagination";
 
 const Table = () => {
   const cryptoCoins = useAppSelector((store) => store.cryptoCoins.coins);
+
+  const { totalCount, currentPage, limit, siblingCount } = useAppSelector(
+    (store) => store.pagination
+  );
+
+  const paginationRange: (number | string)[] | undefined = usePagination({
+    siblingCount,
+    currentPage,
+    limit,
+    total: totalCount,
+  });
   return (
     <div className="table">
       <Table__Container
@@ -16,15 +28,19 @@ const Table = () => {
         rank={TableColumnsEnum.RANK}
         key={uuidv4()}
       />
-      {cryptoCoins.map((coin) => (
-        <Table__Container
-          key={uuidv4()}
-          name={coin.name}
-          price={+coin.priceUsd}
-          symbol={coin.symbol}
-          rank={coin.rank}
-        />
-      ))}
+      {cryptoCoins.map((coin, index) => {
+        if (paginationRange?.includes(index + 1)) {
+          return (
+            <Table__Container
+              key={uuidv4()}
+              name={coin.name}
+              price={+coin.priceUsd}
+              symbol={coin.symbol}
+              rank={coin.rank}
+            />
+          );
+        }
+      })}
     </div>
   );
 };
