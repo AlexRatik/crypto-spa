be9125e94556eee7422c4b4cb0eb5a960814f7de
AlexRatik <aleksandr-ratkevich@mail.ru>
2022-07-28 21:30:54 +0300

    add reducer to store

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,11 +1,13 @@
 import { configureStore, ThunkAction, Action } from "@reduxjs/toolkit";
 import counterReducer from "../features/counter/counterSlice";
 import cryptoCoinsReducer from "../features/cryptoCoins/cryptoCoinsSlice";
+import paginationReducer from "../features/pagination/paginationSlice";
 
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
     cryptoCoins: cryptoCoinsReducer,
+    pagination: paginationReducer,
   },
 });
 
