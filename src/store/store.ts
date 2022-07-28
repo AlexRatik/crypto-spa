a0f4988c6b55f3580fcdf617c46413d654b0dc0e
AlexRatik <aleksandr-ratkevich@mail.ru>
2022-07-28 17:18:01 +0300

    moved store and hooks

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,9 +1,11 @@
-import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
+import { configureStore, ThunkAction, Action } from "@reduxjs/toolkit";
+import counterReducer from "../features/counter/counterSlice";
+import cryptoCoinsReducer from "../features/cryptoCoins/cryptoCoinsSlice";
 
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
+    cryptoCoins: cryptoCoinsReducer,
   },
 });
 
