aa1b41421d3983a672b3236a4e725e20bcdd351d
AlexRatik <aleksandr-ratkevich@mail.ru>
2022-07-29 23:18:46 +0300

    fix coinPage

--- src/pages/coinPage/coinPage.tsx
+++ src/pages/coinPage/coinPage.tsx
@@ -1,13 +1,23 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useParams } from "react-router-dom";
-import { useAppSelector } from "../../hooks/hooks";
+import { useAppDispatch, useAppSelector } from "../../hooks/hooks";
 import CoinDescription from "../../components/coinDescription/coinDescription";
+import { getCryptoCoinHistory } from "../../features/cryptoCoins/cryptoCoinsSlice";
+import Chart from "../../components/chart/chart";
 
 const CoinPage = () => {
+  const dispatch = useAppDispatch();
+
   const { id } = useParams();
 
-  const allCoins = useAppSelector((store) => store.cryptoCoins.coins);
+  useEffect(() => {
+    dispatch(getCryptoCoinHistory({ id: String(id), interval: "d1" }));
+  }, [id]);
 
+  const allCoins = useAppSelector((store) => store.cryptoCoins.coins);
+  const historyData = useAppSelector((store) => store.cryptoCoins.coinHistory);
+  const historyLength = historyData.length;
+  const history = historyData.slice(historyLength - 31);
   const coin = allCoins.find((cryptoCoin) => cryptoCoin.id === id);
 
   if (!coin) return null;
@@ -15,6 +25,7 @@ const CoinPage = () => {
   return (
     <div>
       <CoinDescription coin={coin} />
+      <Chart data={history} />
     </div>
   );
 };
