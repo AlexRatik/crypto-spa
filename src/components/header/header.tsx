26cfa66bcd3b5ef6ae16f50dbd2a86d86df309d8
AlexRatik <aleksandr-ratkevich@mail.ru>
2022-07-29 19:15:15 +0300

    fix header

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -3,13 +3,22 @@ import CurrencyTableau from "../currencyTableau/currencyTableau";
 import "./header.scss";
 import Button from "../button/button";
 import PortfolioValue from "../portfolioValue/portfolioValue";
+import { useAppDispatch } from "../../hooks/hooks";
+import { showPortfolio } from "../../features/portfolio/portfolioSlice";
 
 const Header = () => {
+  const dispatch = useAppDispatch();
   return (
     <div className="header">
       <CurrencyTableau />
       <PortfolioValue />
-      <Button text={"Portfolio"} clickHandler={() => {}} className={""} />
+      <Button
+        text={"Portfolio"}
+        clickHandler={() => {
+          dispatch(showPortfolio());
+        }}
+        className={""}
+      />
     </div>
   );
 };
