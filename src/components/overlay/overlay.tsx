6d06fc2900b442e723538ebf9e81600873f7f9d6
AlexRatik <aleksandr-ratkevich@mail.ru>
2022-07-29 19:15:02 +0300

    fix overlay

--- src/components/overlay/overlay.tsx
+++ src/components/overlay/overlay.tsx
@@ -2,14 +2,16 @@ import React from "react";
 import "./overlay.scss";
 import { useAppDispatch } from "../../hooks/hooks";
 import { hideModal } from "../../features/modalForBuyCoins/modalForBuyCoinsSlice";
+import { hidePortfolio } from "../../features/portfolio/portfolioSlice";
 
 const Overlay = () => {
   const dispatch = useAppDispatch();
   const clickHandler = () => {
     dispatch(hideModal());
+    dispatch(hidePortfolio());
   };
 
-  return <div className="overlay" onClick={clickHandler}></div>;
+  return <div className="overlay" onClick={clickHandler} />;
 };
 
 export default Overlay;
