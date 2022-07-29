eae2a4eabbca94edacaa486b6eec69b771aafd7c
AlexRatik <aleksandr-ratkevich@mail.ru>
2022-07-29 23:17:51 +0300

    fix cryptoCoinsSlice

--- src/features/cryptoCoins/cryptoCoinsSlice.ts
+++ src/features/cryptoCoins/cryptoCoinsSlice.ts
@@ -3,15 +3,19 @@ import { ICoin } from "./ICoin";
 import { fetchCoins } from "./fetchCoins";
 import { getPopularCoins } from "./getPopularCoins";
 import { setTotalCount } from "../pagination/paginationSlice";
+import { fetchHistory } from "./fetchHistory";
+import { IHistory } from "./IHistory";
 
 interface IInitialState {
   coins: ICoin[];
   popularCoins: ICoin[];
+  coinHistory: IHistory[];
 }
 
 const initialState: IInitialState = {
   coins: [],
   popularCoins: [],
+  coinHistory: [],
 };
 
 export const getCryptoCoins = createAsyncThunk(
@@ -24,6 +28,14 @@ export const getCryptoCoins = createAsyncThunk(
   }
 );
 
+export const getCryptoCoinHistory = createAsyncThunk(
+  "cryptoCoins/getCoinHistory",
+  async ({ id, interval }: { id: string; interval: string }, { dispatch }) => {
+    const data = await fetchHistory(id, interval);
+    dispatch(setCoinHistory(data));
+  }
+);
+
 export const cryptoCoinsSlice = createSlice({
   name: "cryptoCoins",
   initialState,
@@ -31,12 +43,15 @@ export const cryptoCoinsSlice = createSlice({
     setCryptoCoins: (state, action: PayloadAction<ICoin[]>) => {
       state.coins = action.payload;
     },
-    setPopularCryptoCoins: (state, action) => {
+    setPopularCryptoCoins: (state, action: PayloadAction<ICoin[]>) => {
       state.popularCoins = getPopularCoins(action.payload);
+    },
+    setCoinHistory: (state, action: PayloadAction<IHistory[]>) => {
+      state.coinHistory = action.payload;
     },
   },
 });
 
-export const { setCryptoCoins, setPopularCryptoCoins } =
+export const { setCryptoCoins, setPopularCryptoCoins, setCoinHistory } =
   cryptoCoinsSlice.actions;
 export default cryptoCoinsSlice.reducer;
