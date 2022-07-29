7afb1cd549aae5f53f657bf31c10f8be725e90d1
AlexRatik <aleksandr-ratkevich@mail.ru>
2022-07-29 09:30:47 +0300

    create build

--- src/components/portfolioValue/portfolioValue.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "./portfoliaValue.scss";
-
-const PortfolioValue = () => {
-  return <div className="portfolioValue">134,32 USD +2,38 (1,80 %)</div>;
-};
-
-export default PortfolioValue;
--- src/components/table/tableColumnsEnum.ts
+++
@@ -1,6 +0,0 @@
-export enum TableColumnsEnum {
-  NAME = "NAME",
-  SYMBOL = "SYMBOL",
-  PRICE_USD = "PRICE(USD)",
-  RANK = "RANK",
-}
